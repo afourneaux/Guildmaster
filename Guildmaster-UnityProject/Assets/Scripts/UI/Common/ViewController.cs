deab3f190491c6552cdd6133213217d8ede890e7
DavidIvanov <41933470+DavidIvanov@users.noreply.github.com>
2021-05-06 16:56:18 -0400

    ViewController and ViewManager classes set up

---
+++ Guildmaster-UnityProject/Assets/Scripts/UI/Common/ViewController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public struct ViewParameters {
+    public string viewName;
+    public string viewSource;
+}
+
+public class ViewController : MonoBehaviour
+{
+    /*
+     Base class for all views
+    */
+
+    protected ViewManager viewManager; //used to boot up views
+    protected string viewName;
+    public string ViewName   // property
+    {
+        get { return viewName; }   // get method
+    }    
+    protected string source; //tells you from where the view was called
+    public string Source   // property
+    {
+        get { return source; }   // get method
+        set { source = value; }  // set method
+    }
+
+    protected virtual void Intialize()
+    {
+
+    }
+
+    protected virtual async Task IntializeAsync() 
+    {
+
+    }
+
+    void OnEnable() 
+    {
+        OnShown();
+        OnShownAsync();
+    }
+
+    protected virtual void OnShown()
+    {
+
+    }
+
+    protected virtual async Task OnShownAsync()
+    {
+
+    }
+
+    void OnDisable()
+    {
+        OnHidden();
+        OnHiddenAsync();
+    }
+
+    protected virtual void OnHidden()
+    {
+
+    }
+
+    protected virtual async Task OnHiddenAsync()
+    {
+
+    }
+
+    protected virtual void Refresh()
+    {
+
+    }
+
+    protected virtual async Task RefreshAsync()
+    {
+
+    }
+
+    private void MakeDirty()
+    {
+        Refresh();
+        RefreshAsync();
+    }
+
+    private void Back() //go back to source view
+    {
+        Debug.Log(ViewName + "closed, Go back to " + Source);
+        //close view
+        gameObject.SetActive(false); 
+        //open the source view
+        if(string.IsNullOrEmpty(Source))
+        {
+            viewManager.ShowView(new ViewParameters{ viewName = Source, viewSource = "" });
+        }
+    }
+
+
+
+}
---
+++ Guildmaster-UnityProject/Assets/Scripts/UI/Common/ViewManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ViewManager : MonoBehaviour
+{
+    /*
+     Do not destroy object.
+     */
+
+    [SerializeField] List<ViewController> views; 
+
+    public void ShowView(ViewParameters viewParameters)
+    { 
+        //might have to change the way its structure these
+        foreach(ViewController view in views){
+            if(viewParameters.viewName.Equals(view.ViewName))
+            {
+                if(!string.IsNullOrEmpty(viewParameters.viewSource))
+                {
+                    view.Source = viewParameters.viewSource;
+                }
+                view.gameObject.SetActive(true); 
+                return;
+            }
+        }
+    }
+
+
+
+
+}
